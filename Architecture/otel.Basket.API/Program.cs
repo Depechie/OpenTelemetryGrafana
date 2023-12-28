847f36d19ef26d6c0cc8b5dbbc90cc3c323acc60
Glenn Versweyveld <glenn.versweyveld@gmail.com>
2023-12-28 16:02:47 +0100

    Filter structured logs

--- Architecture/otel.Basket.API/Program.cs
+++ Architecture/otel.Basket.API/Program.cs
@@ -13,21 +13,23 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient<ICatalogService, CatalogService>();
 
-var hostBuilder = builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
-        .ReadFrom.Configuration(hostingContext.Configuration));
-
 var otelEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
 
 if(!string.IsNullOrWhiteSpace(otelEndpoint))
-    hostBuilder.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.WriteTo.OpenTelemetry(options =>
-    {
-        options.Endpoint = $"{otelEndpoint}/v1/logs";
-        options.Protocol = Serilog.Sinks.OpenTelemetry.OtlpProtocol.Grpc;
-        options.ResourceAttributes = new Dictionary<string, object>
+    builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(hostingContext.Configuration)
+        .WriteTo.OpenTelemetry(options =>
         {
-            ["service.name"] = builder.Configuration["OTEL_SERVICE_NAME"]
-        };
-    }));
+            options.Endpoint = $"{otelEndpoint}/v1/logs";
+            options.Protocol = Serilog.Sinks.OpenTelemetry.OtlpProtocol.Grpc;
+            options.ResourceAttributes = new Dictionary<string, object>
+            {
+                ["service.name"] = builder.Configuration["OTEL_SERVICE_NAME"]
+            };
+        }));
+else
+    builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(hostingContext.Configuration));
 
 var app = builder.Build();
 
