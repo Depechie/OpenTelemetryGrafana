a0881b5df98bcc2ba953930fe5be70a41345f65f
Glenn Versweyveld <glenn.versweyveld@gmail.com>
2023-12-29 16:48:01 +0100

    Added OTEL - Tempo

--- Architecture/otel.AppHost/Program.cs
+++ Architecture/otel.AppHost/Program.cs
@@ -9,12 +9,20 @@ var loki = builder.AddContainer("loki", "grafana/loki", "2.9.2")
     .WithVolumeMount("loki", "/data/loki", VolumeMountType.Named)
     .WithArgs("-config.file=/etc/loki/local-config.yaml");
 
+var tempo = builder.AddContainer("tempo", "grafana/tempo", "2.3.1")
+    .WithServiceBinding(containerPort: 3200, hostPort: 3200, name: "http", scheme: "http")
+    .WithServiceBinding(containerPort: 4317, hostPort: 4007, name: "otlp", scheme: "http")
+    .WithVolumeMount("../config/tempo.yml", "/etc/tempo.yaml", VolumeMountType.Bind)
+    .WithVolumeMount("tempo", "/tmp/tempo", VolumeMountType.Named)
+    .WithArgs("-config.file=/etc/tempo.yaml");
+
 var otel = builder.AddContainer("otel", "otel/opentelemetry-collector-contrib", "0.91.0")
     .WithServiceBinding(containerPort: 4317, hostPort: 4317, name: "grpc", scheme: "http") // Have to put the schema to HTTP otherwise the C# will complain about the OTEL_EXPORTER_OTLP_ENDPOINT variable
     .WithServiceBinding(containerPort: 55679, hostPort: 9200, name: "zpages", scheme: "http")
     .WithVolumeMount("../config/otel.yml", "/etc/otel-collector-config.yaml", VolumeMountType.Bind)
     .WithArgs("--config=/etc/otel-collector-config.yaml")
     .WithLokiPushUrl("LOKI_URL", loki.GetEndpoint("http"))
+    .WithEnvironment("TEMPO_URL", tempo.GetEndpoint("otlp"))
     .WithDashboardEndpoint("DASHBOARD_URL");
 
 builder.AddContainer("grafana", "grafana/grafana", "10.2.1")
