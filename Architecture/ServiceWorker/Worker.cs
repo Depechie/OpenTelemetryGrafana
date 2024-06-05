e523d5b461b13014d999d5a75957166ee22241a9
Glenn Versweyveld <glenn.versweyveld@gmail.com>
2024-06-05 22:13:03 +0200

    Alignment

--- Architecture/ServiceWorker/Worker.cs
+++ Architecture/ServiceWorker/Worker.cs
@@ -18,39 +18,39 @@ using RabbitMQ.Client.Events;
 namespace ServiceWorker
 {
     public class Worker : BackgroundService
-	{
-		private readonly ILogger<Worker> _logger;
-		private readonly IBus _bus;
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IBus _bus;
 
-		//Important: The name of the Activity should be the same as the name of the Source added in the Web API startup AddOpenTelemetryTracing Builder
-		private static readonly ActivitySource Activity = new("APITracing");
-		private static readonly TextMapPropagator Propagator = new TraceContextPropagator();
+        //Important: The name of the Activity should be the same as the name of the Source added in the Web API startup AddOpenTelemetryTracing Builder
+        private static readonly ActivitySource Activity = new("APITracing");
+        private static readonly TextMapPropagator Propagator = new TraceContextPropagator();
 
-		public Worker(ILogger<Worker> logger)
-		{
-			_logger = logger;
-			_bus = RabbitMQFactory.CreateBus(BusType.DockerNetworkHost);
-		}
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+            _bus = RabbitMQFactory.CreateBus(BusType.DockerNetworkHost);
+        }
 
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-			await _bus.ReceiveAsync<LocationRequest>(QueueType.Processing, (message, args) =>
-			{
-				Task.Run(() => { ProcessMessage(message, args); }, cancellationToken);
-			});
+            await _bus.ReceiveAsync<LocationRequest>(QueueType.Processing, (message, args) =>
+            {
+                Task.Run(() => { ProcessMessage(message, args); }, cancellationToken);
+            });
         }
 
         private void ProcessMessage(LocationRequest message, BasicDeliverEventArgs args)
         {
-			var parentContext = Propagator.Extract(default, args.BasicProperties, ExtractTraceContextFromBasicProperties);
-			Baggage.Current = parentContext.Baggage;
+            var parentContext = Propagator.Extract(default, args.BasicProperties, ExtractTraceContextFromBasicProperties);
+            Baggage.Current = parentContext.Baggage;
 
-			using (var activity = Activity.StartActivity("Process Message", ActivityKind.Consumer, parentContext.ActivityContext))
-			{
+            using (var activity = Activity.StartActivity("Process Message", ActivityKind.Consumer, parentContext.ActivityContext))
+            {
                 AddActivityTags(activity);
                 _logger.LogInformation($"Message received location: {message.Latitude} - {message.Longitude}");
-			}
-		}
+            }
+        }
 
         private IEnumerable<string> ExtractTraceContextFromBasicProperties(IBasicProperties props, string key)
         {
